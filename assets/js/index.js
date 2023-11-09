9331706eb13f68186e1ad1a3622088ce6e68217f
Kyrylo <fresh.js.ftd.@gmail.com>
2023-11-09 19:56:53 +0200

    Add primitive data type examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,39 @@
 // однорядковий коментар
 /*
   багаторядковий коментар
+
+  alert - функція
+  alert() - запуск функції
+  'Hello World' - тип даних Рядок / string
+  ; - кінець інструкції
 */
-alert('Hello World');
+// alert('Hello World');
+
+// String
+'asdmsa;lds 213 342343  44po43j43p';
+"asdkhsakljdhsjfhdsfjdk jfdweklfe";
+
+`sdnsdlsndl nl n234 9403 `;
+
+// Number
+10000000000000000000000000000000000000000000000000000000000000000000;
+2.5;
+-50;
+
+// BigInt
+4543773587373786786757434735473555252525253535272525252250000000000000000004545n;
+
+// Boolean
+true; // правда / працює
+false; // брехня / непрацює
+
+// null
+null;
+
+// undefined
+undefined;
+
+// Symbol
+Symbol('опис');
+
+// alert( typeof null );
