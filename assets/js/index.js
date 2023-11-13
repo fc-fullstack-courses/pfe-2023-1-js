5a6e0eb8feff0720f36f140d3baa3cb4acfb0beb
Kyrylo <fresh.js.ftd.@gmail.com>
2023-11-13 19:20:42 +0200

    Add advanced sum of 2 numbers example

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,8 +18,44 @@
 // Отримати від користувача його ім'я
 // Привитатися з ним по імені, наприклад: Привіт, Кирило
 
-let userName = prompt('Введіть ваше ім`я');
+// let userName = prompt('Введіть ваше ім`я');
 
-let greetingText = 'Привіт, ' + userName;
+// let greetingText = 'Привіт, ' + userName;
 
-alert(greetingText);
+// alert(greetingText);
+
+
+// Отримати від користувача 2 числа
+// вивести на екран їх суму
+
+// v1
+// let num1 = prompt('Введіть перше число');
+// let num2 = prompt('Введіть друге число');
+
+// alert(+num1 + +num2);
+
+
+// v2
+// let input1 = prompt('Введіть перше число');
+// let input2 = prompt('Введіть друге число');
+
+// let num1 = +input1;
+// let num2 = +input2;
+
+// alert(num1 + num2);
+
+// v3
+// let num1 = +prompt('Введіть перше число');
+// let num2 = +prompt('Введіть друге число');
+
+// alert(num1 + num2);
+
+// fool protection version
+let num1 = +prompt('Введіть перше число');
+let num2 = +prompt('Введіть друге число');
+
+let result = num1 + num2;
+
+if(result === result) {
+  alert(result);
+}
