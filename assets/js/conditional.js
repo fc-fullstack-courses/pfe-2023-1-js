f30de971f15f570e8dc9dff5c29bd7bc1e0c840a
Kyrylo <fresh.js.ftd.@gmail.com>
2023-11-14 20:37:37 +0200

    Add else if example

--- assets/js/conditional.js
+++ assets/js/conditional.js
@@ -24,6 +24,27 @@ if (userName) {
   console.log("Hello Guest");
 }
 
+let directorUserName = "Bohdan";
+let managerName = "Inna";
+let userName2 = "Kyrylo";
+
+debugger;
+
+if (userName2 === directorUserName) {
+
+  // вітаємось шановливо якщо ім'я співпадає з директором
+  console.log("Good day " + userName2);
+} else if (userName2 === managerName) {
+  // особливо вітаємося з менеджером
+  console.log("Greetings " + userName2);
+} else if (userName2) {
+  // коли ім'я приводиться до істини
+  console.log("Hello " + userName2);
+} else {
+  // у всіх інших випадках
+  console.log("Hello Guest");
+}
+
 // if (!userName) {
 //   console.log("Hello Guest");
 // }
