de243dfa2ac4c27ddd9c1591bf1eb87f7a968931
Kyrylo <fresh.js.ftd.@gmail.com>
2023-11-14 20:18:37 +0200

    Add if...else example

---
+++ assets/js/conditional.js
@@ -0,0 +1,29 @@
+// if ... else
+
+let expression = 5 < 0;
+
+if (expression) {
+  //
+  console.log("щось");
+  console.log("щось2");
+}
+
+let expression1 = false;
+
+if (expression1) console.log("щось");
+
+console.log("test");
+
+let userName = "";
+
+if (userName) {
+  // коли ім'я приводиться до істини
+  console.log("Hello " + userName);
+} else {
+  // у всіх інших випадках
+  console.log("Hello Guest");
+}
+
+// if (!userName) {
+//   console.log("Hello Guest");
+// }
