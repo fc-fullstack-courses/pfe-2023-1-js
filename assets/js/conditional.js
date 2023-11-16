ee68a482696b8e1c480ebe27fabb48ff3601e9f0
Kyrylo <fresh.js.ftd.@gmail.com>
2023-11-16 18:39:14 +0200

    Add ternary operator

--- assets/js/conditional.js
+++ assets/js/conditional.js
@@ -28,7 +28,7 @@ let directorUserName = 'Bohdan';
 let managerName = 'Inna';
 let userName2 = 'Kyrylo';
 
-debugger;
+// debugger;
 
 if (userName2 === directorUserName) {
   // вітаємось шановливо якщо ім'я співпадає з директором
@@ -47,3 +47,50 @@ if (userName2 === directorUserName) {
 // if (!userName) {
 //   console.log("Hello Guest");
 // }
+
+// const userName3 = prompt("Введіть ім'я");
+
+// if (userName3) {
+//   // дія 1
+//   alert('Hello ' + userName3);
+// } else {
+//   // дія 2
+//   alert('Hello Guest');
+// }
+
+// тернарка
+
+/*
+  умова ? дія 1 : дія 2
+*/
+
+// userName3 ? alert('Hello ' + userName3) : alert('Hello Guest');
+
+10 > 0 ? console.log('10 > 0 is true') : console.log('10 > 0 is false');
+
+let value;
+
+if (10 > 0) {
+  value = 'This is true';
+} else {
+  value = 'This is false';
+}
+
+const value2 = 10 > 0 ? 'This is true' : 'This is false';
+
+let price1 = 150;
+let price2 = 50;
+
+let accountBalance = 100;
+
+if (accountBalance >= price1) {
+  // alert('Товар придбано');
+} else {
+  // alert('Не вистачає грошей');
+}
+
+// accountBalance >= price1 ? alert('Товар придбано') : alert('Не вистачає грошей');
+
+alert(accountBalance >= price2 ? 'Товар придбано' : 'Не вистачає грошей');
+
+100 > 10 ? 'Yes' : 100 === 10 ? 'Yes2' : 'No';
