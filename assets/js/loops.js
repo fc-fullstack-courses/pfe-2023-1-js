9d94a4443743adc23aa1c792dd961056112e8e69
Kyrylo <fresh.js.ftd.@gmail.com>
2023-11-16 20:34:53 +0200

    Add minitask and continue example

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -26,15 +26,47 @@ while (count < 4) {
   console.log('Iteration');
 }
 
-let count2 = 0;
+// let count2 = 0;
 
-while (true) {
-  count2++;
-  console.log('Iteration ' + count2);
-  if (count2 > 10) {
-    debugger;
-    // дострокове завершення циклу
-    break;
+// while (true) {
+//   count2++;
+//   console.log('Iteration ' + count2);
+//   if (count2 > 10) {
+//     debugger;
+//     // дострокове завершення циклу
+//     break;
+//   }
+//   console.log('test');
+// }
+
+/*
+  зробити цикл, під час виконання якого у консоль будуть виводитися цілі парні числа до 10
+  0,2,4,6,8,10
+*/
+
+let counter = 0;
+
+// while(counter <= 10) {
+//   console.log('iteration');
+//   if(counter % 2 === 0) console.log(counter);
+//   counter++;
+// }
+// debugger;
+
+while(counter <= 10) {
+  if(counter % 2 !== 0) {
+    counter++;
+    // дострокове завершення поточної ітерації (кола)
+    continue;
   }
-  console.log('test');
+
+  console.log(counter);
+
+  counter++;
 }
+
+// while(counter <= 10) {
+//   console.log('iteration');
+//   console.log(counter);
+//   counter += 2;
+// }
