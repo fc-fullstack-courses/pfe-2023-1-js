1a9ab043914185e81ea945c05528198db1309e44
Kyrylo <fresh.js.ftd.@gmail.com>
2023-11-28 19:56:52 +0200

    Add type conversion example

---
+++ assets/js/primitives.js
@@ -0,0 +1,29 @@
+// console.log('some string');
+// console.log('some string'.length);
+
+// const str1 = 'asdsad';
+// console.log(str1);
+
+// const str2 = new String('asdsad');
+// console.log(str2);
+// console.log(str2.length);
+
+// const bool1 = new Boolean(false);
+
+// console.log( bool1 ? 'test' : 'cat');
+
+// const empty = {};
+
+// if (empty) {
+//   console.log('its empty');
+// }
+
+// приводить значення до булевого
+const bool2 = Boolean(false); // !!
+
+const bool3 = Boolean(NaN);
+
+const num1 = Number('asfddsgdsfs'); // +
+
+const str1 = String(500); // '' + 500
+
