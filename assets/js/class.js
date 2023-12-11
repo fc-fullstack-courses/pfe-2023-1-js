1ada615c0115764a14e569e671823d42b48321f7
Kyrylo <fresh.js.ftd.@gmail.com>
2023-12-11 19:38:01 +0200

    Add User class task with solution

--- assets/js/class.js
+++ assets/js/class.js
@@ -32,20 +32,20 @@ const user2 = new OldUser('John', 'Doe', 42);
 
 // class
 
-class User {
-  constructor(firstName, lastName, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-  }
+// class User {
+//   constructor(firstName, lastName, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//   }
 
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
 
-const user3 = new User('Test', 'User', 5000);
-const user4 = new User('Test', 'User', 5000);
+// const user3 = new User('Test', 'User', 5000);
+// const user4 = new User('Test', 'User', 5000);
 
 class House {
   // тіло класу
@@ -69,3 +69,39 @@ class House {
 
 const house1 = new House(3, 'Somwhere is Zaporizhzhia');
 // const house2 = House(3, 'Somwhere is Zaporizhzhia'); // ERROR !!!
+
+/*
+  Створіть клас User
+    який має мати насутпні властивості:
+      ім'я
+      прізвище
+      стать
+      вік
+      зріст
+
+    у екземплярів класу мають бути наступні методи:
+      отримати всі дані про користувача рядком
+      метод isAdult - який каже чи є конкретний користувач повнолітнім
+
+*/
+
+class User {
+  constructor(firstName, lastName, age, isMale, height) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.isMale = isMale;
+    this.height = height;
+  }
+
+  getAllUserData() {
+    `User ${this.firstName} ${this.lastName} age: ${this.age} height: ${
+      this.height
+    } m gender: ${this.isMale ? 'male' : 'female'}`;
+  }
+
+  isAdult () {
+    return this.age >= 18;
+  }
+}
+
