6b5330d0ca929bc6f919cdcd633c97ae13202b5c
Kyrylo <fresh.js.ftd.@gmail.com>
2023-12-11 18:56:47 +0200

    Add House example

--- assets/js/class.js
+++ assets/js/class.js
@@ -1,7 +1,8 @@
+'use strict';
 function OldUser(firstName, lastName, age) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.age = age;
+  this.alert = age;
 }
 
 // const userProto = {
@@ -29,20 +30,42 @@ const user1 = new OldUser('John', 'Doe', 42);
 // user1.__proto__ = userProto;
 const user2 = new OldUser('John', 'Doe', 42);
 
-
 // class
 
 class User {
-  constructor (firstName, lastName, age) {
+  constructor(firstName, lastName, age) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
   }
 
-  getFullName () {
+  getFullName() {
     return `${this.firstName} ${this.lastName}`;
   }
 }
 
-const user3 = new User ('Test', 'User', 5000);
-const user4 = new User ('Test', 'User', 5000);
+const user3 = new User('Test', 'User', 5000);
+const user4 = new User('Test', 'User', 5000);
+
+class House {
+  // тіло класу
+  // завжди в суворому режимі !
+
+  // метод конструктору - аналог старого конструктору
+  constructor(numberOfFloors = 1, address = 'CHANGE ME PLZ', style = 'modern') {
+    this.numberOfFloors = numberOfFloors;
+    this.address = address;
+    this.style = style;
+  }
+
+  // нижче можна написати методи екземпляру будинку
+  getHouseData() {
+    return `House data address: ${this.address} floors: ${this.numberOfFloors} architectural style: ${this.style}`;
+  }
+
+  // властивість прототипу
+  isBuilt = true;
+}
+
+const house1 = new House(3, 'Somwhere is Zaporizhzhia');
+// const house2 = House(3, 'Somwhere is Zaporizhzhia'); // ERROR !!!
