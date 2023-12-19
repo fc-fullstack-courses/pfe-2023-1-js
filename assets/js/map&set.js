43c585a4aaa7cc6b07d30ad2bc45f0e7896e0849
Kyrylo <fresh.js.ftd.@gmail.com>
2023-12-19 20:53:35 +0200

    Add vocabulary example

--- assets/js/map&set.js
+++ assets/js/map&set.js
@@ -5,7 +5,7 @@
 const map1 = new Map();
 
 // додавання значень до мапи
-map1.set('test', [1,23,4,5]);
+map1.set('test', [1, 23, 4, 5]);
 map1.set(123, 'number');
 map1.set('123', 'test 1');
 map1.set(123, 'something else');
@@ -35,3 +35,54 @@ map1.forEach(function (value, key, map) {
   console.log(key);
   console.log(map);
 });
+
+/*
+  створимо перекладач 
+  функція яка приймає рядок на одній мові і повертає його "переклад" на іншій
+*/
+
+const vocabulary = new Map();
+
+vocabulary.set('cat', 'кіт');
+vocabulary.set('gray', 'сірий');
+vocabulary.set('mouse', 'миша');
+vocabulary.set('hunted', 'полював');
+vocabulary.set('vase', 'ваза');
+vocabulary.set('broken', 'розбитий');
+vocabulary.set('but', 'але');
+vocabulary.set('pickaxe', 'кірка');
+vocabulary.set('with', 'з допомогою');
+
+const testString = 'Cat hunted mouse but broke pickaxe with vase';
+const testString2 = 'le Gato huntedo mousedo broke pickaxe with vase';
+
+function translate(str, vocab = vocabulary) {
+  const lowerCaseStr = str.toLowerCase();
+
+  const words = lowerCaseStr.split(' ');
+
+  const translatedWords = [];
+
+  for (const word of words) {
+    // 1 отримати українську версію
+    // 2 покласти її в масив translatedWords
+    // 3 якщо української версії не існує то покласти у масив англійську
+    if (vocab.has(word)) {
+      const translatedWord = vocab.get(word);
+      translatedWords.push(translatedWord);
+    } else {
+      translatedWords.push(word);
+    }
+  }
+
+  const translatedString = translatedWords.join(' ');
+
+  return translatedString;
+}
+
+const translate2 = (str, vocab = vocabulary) =>
+  str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => (vocab.has(word) ? vocab.get(word) : word))
+    .join(' ');
