ecb3e45b748e1ccb5f029847360904300a0ad4a3
Kyrylo <fresh.js.ftd.@gmail.com>
2023-12-20 14:21:58 +0200

    Add O(n log n) algo example

--- assets/js/algorythms.js
+++ assets/js/algorythms.js
@@ -144,3 +144,57 @@ function binarySearch (arr, value) {
     }
   }
 }
+
+// O(N * log N)
+// сортування злиттям
+
+const arr = [654, 98, 321, -50056, 0, 1, 5];
+
+/*
+  загальний алгоритм:
+    1 ділимо масив навпіл доти, доки не отримаємо багато (N) масивів розміром 1 або 0
+
+    2 зливаємо в один масив 2 існуючих відсортованих масиви, по черзі порівнюючи елементи в них
+*/
+
+// допоміжня функція для 2 кроку
+function merge(leftArr, rightArr) {
+  // debugger;
+  // 1. створимо відсортований масив
+  let sortedArr = [];
+
+  // 2. ходимо по масивам leftArr і rightArr поки в них є елементи
+  // на кожній ітерації знаходимо найменший елемент з цих масівів і вставляємо його в sortedArr
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      const smallestNum = leftArr.shift();
+      sortedArr.push(smallestNum);
+    } else {
+      const smallestNum = rightArr.shift();
+      sortedArr.push(smallestNum);
+    }
+  }
+
+  // 3 створюємо результуючий масив у який кладемо спочатку все з sortedArr
+  // потім все що залишилось у leftArr і rightArr
+  const resultArr = [...sortedArr, ...leftArr, ...rightArr];
+  return resultArr;
+}
+
+function mergeSort(arr = []) {
+  // debugger;
+  // базовий випадок
+  if (arr.length <= 1) return arr;
+
+  let middleIndex = Math.floor (arr.length / 2);
+
+  // розбиваємо масив на 2 підмасиви приблизно навпіл
+  const leftArr = arr.slice(0, middleIndex);
+  const rightArr = arr.slice(middleIndex);
+
+  // рекурсивна частина
+  const leftSortedArr = mergeSort(leftArr);
+  const rightSortedArr = mergeSort(rightArr);
+
+  return merge(leftSortedArr, rightSortedArr);
+}
