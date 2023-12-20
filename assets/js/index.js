e7db691f21a485bdff68cdd30ef17425b78fb3cc
Kyrylo <fresh.js.ftd.@gmail.com>
2023-12-20 14:21:05 +0200

    Add O(n log n) example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,11 @@
-
 class Basic {
-  test () {
+  test() {
     console.log('basic test');
   }
 }
 
 class Other extends Basic {
-  test () {
+  test() {
     console.log('basic test');
   }
 }
@@ -14,7 +13,7 @@ class Other extends Basic {
 class Advanced extends Basic {
   test() {} // BAD
 
-  get value () {
+  get value() {
     return 10;
   }
 }
@@ -27,11 +26,11 @@ const obj = {
   key1: 'value 1',
   key2: 'value 2',
   [symbolKey]: 'value 3 IMPORTANT',
-}
+};
 
 console.log(obj);
 
-for(const key in obj) {
+for (const key in obj) {
   console.log(key);
   obj[key] = 'something new';
 }
@@ -45,7 +44,6 @@ console.log(obj);
   console.log('function executed');
 })(213232);
 
-
 // (function ($){
 //     let counter = 1;
 //     $("#increment").click(function () {
@@ -53,5 +51,56 @@ console.log(obj);
 //     })
 // })(jQuery)
 
+// O(N * log N)
+// сортування злиттям
 
+const arr = [654, 98, 321, -50056, 0, 1, 5];
 
+/*
+  загальний алгоритм:
+    1 ділимо масив навпіл доти, доки не отримаємо багато (N) масивів розміром 1 або 0
+
+    2 зливаємо в один масив 2 існуючих відсортованих масиви, по черзі порівнюючи елементи в них
+*/
+
+// допоміжня функція для 2 кроку
+function merge(leftArr, rightArr) {
+  // debugger;
+  // 1. створимо відсортований масив
+  let sortedArr = [];
+
+  // 2. ходимо по масивам leftArr і rightArr поки в них є елементи
+  // на кожній ітерації знаходимо найменший елемент з цих масівів і вставляємо його в sortedArr
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      const smallestNum = leftArr.shift();
+      sortedArr.push(smallestNum);
+    } else {
+      const smallestNum = rightArr.shift();
+      sortedArr.push(smallestNum);
+    }
+  }
+
+  // 3 створюємо результуючий масив у який кладемо спочатку все з sortedArr
+  // потім все що залишилось у leftArr і rightArr
+  const resultArr = [...sortedArr, ...leftArr, ...rightArr];
+  return resultArr;
+}
+
+function mergeSort(arr = []) {
+  // debugger;
+  // базовий випадок
+  if (arr.length <= 1) return arr;
+
+  let middleIndex = Math.floor (arr.length / 2);
+
+  // розбиваємо масив на 2 підмасиви приблизно навпіл
+  const leftArr = arr.slice(0, middleIndex);
+  const rightArr = arr.slice(middleIndex);
+
+  // рекурсивна частина
+  const leftSortedArr = mergeSort(leftArr);
+  const rightSortedArr = mergeSort(rightArr);
+
+  return merge(leftSortedArr, rightSortedArr);
+}
